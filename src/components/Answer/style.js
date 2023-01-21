8122b2427be6a2b0fa53cb5ac3de933d1d6f4c35
Laís Martins <silveiramartinslais@gmail.com>
2023-01-21 20:33:10 -0300

    feat: answer component created

--- src/components/Answer/style.js
+++ src/components/Answer/style.js
@@ -1,3 +1,38 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const StyledAnswer = styled.div``;
+export const StyledAnswer = styled.div`
+  display: flex;
+
+  align-items: center;
+
+  height: 27px;
+  width: 90vw;
+
+  border-radius: 14px;
+
+  padding: 10px;
+
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 21px;
+
+  ${(props) => {
+    switch (props.status) {
+      default:
+        return css`
+          color: var(--purple-blue);
+          background-color: var(--white);
+        `;
+      case 'right':
+        return css`
+          color: var(--white);
+          background-color: var(--green-right);
+        `;
+      case 'wrong':
+        return css`
+          color: var(--white);
+          background-color: var(--red-wrong);
+        `;
+    }
+  }}
+`;
