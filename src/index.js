bc4795962665ce3aee2eb0caac2cd6297ea88295
Laís Martins <silveiramartinslais@gmail.com>
2023-01-21 17:35:46 -0300

    feat: contextAPI configured

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from './styles/GlobalStyles';
+import { MainProvider } from './contexts/mainContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles />
+    <MainProvider>
+      <App />
+    </MainProvider>
   </React.StrictMode>
 );
 
