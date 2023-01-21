a136111fcd3700ff0e083550764ef482c4d842c2
Laís Martins <silveiramartinslais@gmail.com>
2023-01-21 16:25:08 -0300

    feat: GlobalStyles created

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,57 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+      *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: 0;
+
+        font-family: 'Roboto', sans-serif;
+        font-style: normal;
+    }
+
+    :root{
+        --red: #FF5757;
+        --white: #FFFF;
+        --purple-blue: #1F1347;
+        --yellow: #FFE500;
+        --blue: #00A3FF;
+
+    }
+
+    body {
+        display: flex;
+
+        justify-content: center;
+        align-items: center;
+
+        background-color: var(--red);
+        width: 100vw;
+
+    }
+
+    button {
+        cursor: pointer;
+        transition: 0.5s;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    ul {
+        list-style: none;
+    }
+
+    h1{
+        display: flex;
+
+        justify-content: center;
+        align-items: center;
+
+        color: var(--white);
+        weight: 600;
+        font-size: 22px;
+    }
+`;
