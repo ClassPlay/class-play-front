b7c698365a7d92a4d8dd349f376412b0c126ef4e
Laís Martins <silveiramartinslais@gmail.com>
2023-01-22 04:45:40 -0300

    feat: answer component updated

--- src/components/Answer/index.js
+++ src/components/Answer/index.js
@@ -1,7 +1,28 @@
+import { useContext, useState } from 'react';
 import { StyledAnswer } from './style';
+import { MainContext } from '../../contexts/mainContext';
 
 const Answer = ({ answer }) => {
-  return <StyledAnswer status=''>{answer}</StyledAnswer>;
+  const [status, setStatus] = useState('');
+  const { questions, questionsCounter, setCoinsState, coinsState } =
+    useContext(MainContext);
+
+  const ensureAnswer = () => {
+    if (answer === questions[questionsCounter].respostaCerta) {
+      setStatus('right');
+      setCoinsState(coinsState + questions[questionsCounter].ponto);
+    } else if (answer !== questions[questionsCounter].respostaCerta) {
+      setStatus('wrong');
+    } else {
+      setStatus('');
+    }
+  };
+
+  return (
+    <StyledAnswer onClick={() => ensureAnswer()} status={status}>
+      {answer}
+    </StyledAnswer>
+  );
 };
 
 export default Answer;
