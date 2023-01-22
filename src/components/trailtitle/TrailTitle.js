3be7dda584de7130bfbdcfd6310db0efc9d74edc
Fernando <21fernandinhow@gmail.com>
2023-01-21 21:21:56 -0300

    Created Trail Title

---
+++ src/components/trailtitle/TrailTitle.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyledTrailTitle } from "./style";
+
+function TrailTitle () {
+
+    return(
+
+        <StyledTrailTitle>
+            <h1> MATEMÁTICA </h1>
+        </StyledTrailTitle>
+
+    )
+}
+
+export default TrailTitle;
