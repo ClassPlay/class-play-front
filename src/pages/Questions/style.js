f8bad72a4f264f3559dcb9c1b80ba9c033f0a9c1
Fernando <21fernandinhow@gmail.com>
2023-01-22 02:36:53 -0300

    Created Arrow Button

---
+++ src/pages/Questions/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledQuestions = styled.div`
+
+    .progress-div{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-top: 30px;
+        padding: 20px;
+    }
+
+
+`
