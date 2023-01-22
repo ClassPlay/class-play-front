3365b36cf382ff1860434799401f2ebab5921a65
Fernando <21fernandinhow@gmail.com>
2023-01-21 21:44:16 -0300

    Created trails

---
+++ src/components/trail/style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledTrail = styled.div`
+
+    border-radius: 10px;
+    background-color: var(--white);
+    width: 40%;
+    margin: 10px;
+
+    img{
+        width: 100%;
+        border-radius: 10px 10px 0px 0px;
+    }
+
+    h3{
+        text-align: center;
+        font-size: 1.2rem;
+        margin-top: 5px;
+    }
+
+`
