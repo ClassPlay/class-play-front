730e627a4bf87adf7634e2019724ba2cc7d7c13a
Laís Martins <silveiramartinslais@gmail.com>
2023-01-21 21:50:46 -0300

    feat: page questions updated

--- src/pages/Questions/index.js
+++ src/pages/Questions/index.js
@@ -1,10 +1,12 @@
 import Answer from '../../components/Answer';
+import QuestionHeader from '../../components/QuestionHeader';
 import Header from '../../components/header/header';
 
 function Questions() {
   return (
     <>
       <Header />
+      <QuestionHeader points={10} />
       <Answer answer='teste' />
       <Answer answer='teste' />
     </>
