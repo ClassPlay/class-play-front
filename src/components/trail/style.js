4bd5eeeb8e4fa4c909350e9dfa15e0d71f776813
Laís Martins <silveiramartinslais@gmail.com>
2023-01-22 05:41:13 -0300

    feat: trail component updated

--- src/components/trail/style.js
+++ src/components/trail/style.js
@@ -1,23 +1,21 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const StyledTrail = styled.div`
+  border-radius: 13px;
+  filter: drop-shadow(6px 4px 4px rgba(0, 0, 0, 0.25));
+  background-color: var(--white);
+  width: 40vw;
+  height: 40vw;
+  margin: 10px;
 
-    border-radius: 13px;
-    filter: drop-shadow(6px 4px 4px rgba(0, 0, 0, 0.25));
-    background-color: var(--white);
-    width: 40vw;
-    height: 40vw;
-    margin: 10px;
+  img {
+    width: 100%;
+    border-radius: 10px 10px 0px 0px;
+  }
 
-    img{
-        width: 100%;
-        border-radius: 10px 10px 0px 0px;
-    }
-
-    h3{
-        text-align: center;
-        font-size: 6vw;
-        margin-top: 5px;
-    }
-
-`
+  h3 {
+    text-align: center;
+    font-size: 6vw;
+    margin-top: 5px;
+  }
+`;
