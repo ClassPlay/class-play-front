d48f845bb4d9c7fbe962da9a004ecb6d3ee95f60
Laís Martins <silveiramartinslais@gmail.com>
2023-01-21 23:02:25 -0300

    feat: home page updated

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,13 @@
-import Answer from '../../components/Answer';
+import HomeHeader from '../../components/HomeHeader';
 import Header from '../../components/header/header';
 import Trail from '../../components/trail/trail';
-import { StyledTrailsSection } from "./trails";
+import { StyledTrailsSection } from './trails';
 
 function Home() {
   return (
     <>
       <Header />
+      <HomeHeader />
       <StyledTrailsSection>
         <Trail />
         <Trail />
